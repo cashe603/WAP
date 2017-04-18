89a57708aa921d89a19f1fdf16c18d2ebfe8a851
islamfarhan <farhan.islam@outlook.com>
2017-04-18 13:45:45 +0300

    Prepared Statements

--- action.php
+++ action.php
@@ -1,44 +1,70 @@
 <?php
 session_start();
 include "config.php";
+
 if(isset($_POST["category"])){
-	$category_query = "SELECT * FROM categories";
-	$run_query = mysqli_query($con,$category_query) or die(mysqli_error($con));
-	echo "
-		<div class='nav nav-pills nav-stacked'>
-			<li class='active'><a href='#'><h4>Categories</h4></a></li>
+    $query = "SELECT cat_id, cat_title FROM categories";
+
+if ($stmt = mysqli_prepare($con, $query)) {
+
+    /* execute statement */
+    mysqli_stmt_execute($stmt);
+    
+    echo "
+    <div class='nav nav-pills nav-stacked'>
+    <li class='active'><a href='#'><h4>Brands</h4></a></li>
 	";
-	if(mysqli_num_rows($run_query) > 0){
-		while($row = mysqli_fetch_array($run_query)){
-			$cid = $row["cat_id"];
-			$cat_name = $row["cat_title"];
-			echo "
-					<li><a href='#' class='category' cid='$cid'>$cat_name</a></li>
+
+    /* bind result variables */
+    mysqli_stmt_bind_result($stmt, $cid, $cname);
+
+    /* fetch values */
+    while (mysqli_stmt_fetch($stmt)) {
+        
+    
+    echo "
+        <li><a href='#' class='category' cid='$cid'>$cname</a></li>
 			";
 		}
 		echo "</div>";
-	}
+
+    
+    } 
+
 }
 
-if(isset($_POST["brand"])){
-	$brand_query = "SELECT * FROM brands";
-	$run_query = mysqli_query($con,$brand_query) or die(mysqli_error($con));
-	echo "
-		<div class='nav nav-pills nav-stacked'>
-			<li class='active'><a href='#'><h4>Brands</h4></a></li>
+	
+
+if(isset($_POST["category"])){
+    $query = "SELECT cat_id, cat_title FROM categories";
+
+if ($stmt = mysqli_prepare($con, $query)) {
+
+    /* execute statement */
+    mysqli_stmt_execute($stmt);
+    
+    echo "
+    <div class='nav nav-pills nav-stacked'>
+    <li class='active'><a href='#'><h4>Brands</h4></a></li>
 	";
-	if(mysqli_num_rows($run_query) > 0){
-		while($row = mysqli_fetch_array($run_query)){
-			$bid = $row["brand_id"];
-			$brand_name = $row["brand_title"];
-			echo "
-					<li><a href='#' class='category' cid='$bid'>$brand_name</a></li>
+
+    /* bind result variables */
+    mysqli_stmt_bind_result($stmt, $cid, $cname);
+
+    /* fetch values */
+    while (mysqli_stmt_fetch($stmt)) {
+        
+    
+    echo "
+        <li><a href='#' class='category' cid='$bid'>$cname</a></li>
 			";
 		}
 		echo "</div>";
-	}
-}
 
+    
+    } 
+
+}
 
 
 ?>
