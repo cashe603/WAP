1937c7a15678b5dc055875a1d3f49f2545c743c7
islamfarhan <farhan.islam@outlook.com>
2017-04-23 20:09:32 +0300

    Logout is fully functional

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+ unset($_SESSION['uid']);
+ unset($_SESSION['loggedin']);
+ unset($_SESSION['name']);
+ 
+ 
+ if(session_destroy())
+ {
+  header("Location: index.php");
+ }
+
+?>
